b6ff5b4d8452efbb4870e074712ee3a24d5aa688
Kyle Anderson <kylera1999@aol.com>
2021-05-04 05:20:21 -0500

    third commit

--- script.js
+++ script.js
@@ -8,3 +8,58 @@ document.addEventListener('mousemove', function(e) {
   cursor.style.left = (x+10) + "px";
   cursor.style.top = (y+10) + "px";
 })
+
+
+document.getElementById("eat").addEventListener("click", function(){
+  document.getElementById("eatVideo").style.display = "flex";
+  document.getElementById("drinkVideo").style.display = "none";
+  document.getElementById("sleepVideo").style.display = "none";
+  document.getElementById("poopVideo").style.display = "none";
+  document.getElementById("peeVideo").style.display = "none";
+  document.getElementById("playVideo").style.display = "none";
+ });
+
+ document.getElementById("drink").addEventListener("click", function(){
+   document.getElementById("eatVideo").style.display = "none";
+   document.getElementById("drinkVideo").style.display = "flex";
+   document.getElementById("sleepVideo").style.display = "none";
+   document.getElementById("poopVideo").style.display = "none";
+   document.getElementById("peeVideo").style.display = "none";
+   document.getElementById("playVideo").style.display = "none";
+  });
+
+  document.getElementById("sleep").addEventListener("click", function(){
+    document.getElementById("eatVideo").style.display = "none";
+    document.getElementById("drinkVideo").style.display = "none";
+    document.getElementById("sleepVideo").style.display = "flex";
+    document.getElementById("poopVideo").style.display = "none";
+    document.getElementById("peeVideo").style.display = "none";
+    document.getElementById("playVideo").style.display = "none";
+   });
+
+   document.getElementById("poop").addEventListener("click", function(){
+     document.getElementById("eatVideo").style.display = "none";
+     document.getElementById("drinkVideo").style.display = "none";
+     document.getElementById("sleepVideo").style.display = "none";
+     document.getElementById("poopVideo").style.display = "flex";
+     document.getElementById("peeVideo").style.display = "none";
+     document.getElementById("playVideo").style.display = "none";
+    });
+
+    document.getElementById("pee").addEventListener("click", function(){
+      document.getElementById("eatVideo").style.display = "none";
+      document.getElementById("drinkVideo").style.display = "none";
+      document.getElementById("sleepVideo").style.display = "none";
+      document.getElementById("poopVideo").style.display = "none";
+      document.getElementById("peeVideo").style.display = "flex";
+      document.getElementById("playVideo").style.display = "none";
+     });
+
+     document.getElementById("play").addEventListener("click", function(){
+       document.getElementById("eatVideo").style.display = "none";
+       document.getElementById("drinkVideo").style.display = "none";
+       document.getElementById("sleepVideo").style.display = "none";
+       document.getElementById("poopVideo").style.display = "none";
+       document.getElementById("peeVideo").style.display = "none";
+       document.getElementById("playVideo").style.display = "flex";
+      });
